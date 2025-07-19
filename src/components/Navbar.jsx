dc5a130d8a9fb1624852e13a8027223614800364
lilnerd <joanna1997@gmail.com>
2025-07-19 00:41:16 -0400

    Updated navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,8 +10,7 @@ export const NavBar = ({menuOpen, setMenuOpen}) => {
 			<div className="max-w-5xl mx-auto px-4">
 				<div className="flex justify-between items-center h-16">
 					<a href="#home" className="font-mono text-xl font-bold text-white"> 
-						{" "}
-						JoAnna <span className="text-blue-400">Detweiler</span>{" "}
+						<img src="/favicon.ico" alt="lilnerd.dev logo" className="w-8 h-8" />
 					</a>
 					
 					<div className="w-7 h-5 relative cursor-pointer z-40 md:hidden" onClick={() => setMenuOpen((prev) => !prev)}>
